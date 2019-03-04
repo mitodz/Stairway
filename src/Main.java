68fd7c6ceadb2c946f1b2c2234352049d5b2e882
mitodz <mitodzzz@gmail.com>
2019-03-04 13:53:23 +0300

    8 из 9 тестов работают... не работает 7 1 1 -2 -4 -6 2 2

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,21 @@ public class Main {
 
     public static int getSumStairs(int[] a) {
         int n = a.length;
-        int[] b = new int[n + 2];
+        int[] b = new int[n + 1];
         int m = b.length;
         for (int i = 1; i < m - 1; i++) {
             b[i] = a[i - 1];
         }
         int count = 0;
-        for (int i = 2; i <= n; i++) {
-            if (b[i - 2] > b[i - 1]) {
+        for (int i = 2; i <= n + 1; i++) {
+            if (b[i - 2] >= b[i - 1] || (b[i - 2] >= 0 && b[i - 1]>=0)) {
                 count += b[i - 2];
             } else {
                 count += b[i - 1];
                 i++;
             }
         }
-        return count + b[n];
+        return count + a[n - 1];
     }
 
     public static void main(String[] args) {
